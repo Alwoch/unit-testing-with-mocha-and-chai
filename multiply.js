11e2a111241970d7c17192c43c3820aa933a043e
Alwoch <salwoch@gmail.com>
2022-05-23 10:36:07 +0300

    added multiply function

---
+++ multiply.js
@@ -0,0 +1,8 @@
+function multiply(a,b){
+ return 1;
+}
+
+const product=multiply(5,6);
+console.log(product);
+
+module.exports=multiply;
---
+++ test/test.js
@@ -0,0 +1,13 @@
+
+const { assert } = require("chai");
+const multiply = require("../multiply");
+
+
+describe('test multiplication',function(){
+ it('cycle 1',()=>{})
+ it('cycle 2',()=>{})
+ it('cycle 3',()=>{})
+ it('cycle 4',()=>{})
+ it('cycle 5',()=>{})
+})
+
